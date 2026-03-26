f9aee4e712432140fff8e11c6f65b3e0347ac1dd
duanxy1989 <104289464+duanxy1989@users.noreply.github.com>
2026-03-26 22:57:21 +0800

    Update sensor.py

--- esphome/components/bl0910-m/sensor.py
+++ esphome/components/bl0910-m/sensor.py
@@ -86,6 +86,7 @@ FINAL_VALIDATE_SCHEMA = uart.final_validate_device_schema(
             cv.Required(CONF_ID): cv.use_id(BL0910),
         }
     ),
+    synchronous=True  # <--- 就是加上这行
 )
 async def reset_energy_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg)
